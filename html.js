9deea2de6e74c2f34adadc446201b364269f4e3b
Bowserinator <bowserinator@gmail.com>
2022-07-16 01:57:45 -0400

    Fix import for html

--- html.js
+++ html.js
@@ -1,6 +1,6 @@
 import { createMainPlaylistPage } from './src/download_all.js';
 import Playlist from './src/playlist.js';
-import config from './config.js';
+import { config } from './config.js';
 import signale from 'signale';
 
 signale.pending('Manually generating HTML from data...');
@@ -10,8 +10,10 @@ for (let playlistId of config.playlistIds)
     try {
         const playlist = new Playlist(playlistId);
         playlist.writeHTML();
+        playlists.push(playlist);
         signale.complete(`Finished writing HTML for playlist ${playlistId}`);
     } catch (e) {
         signale.fatal(e);
     }
 createMainPlaylistPage(playlists);
+
